36a6e70a70f6b71ec5ed7a8a9da57860e0990e2d
Arseniy Alexeev <ars@alexeev.info>
2021-09-24 13:45:31 +0300

    fix: Testing blocks separated
    Comparing result file updated

--- Alexeev-B12/src/main.c
+++ Alexeev-B12/src/main.c
@@ -9,22 +9,51 @@
 #define TESTS_AMOUNT 100
 #define ITERATIONS_IN_TEST (MAX_RUN_ITERATIONS / TESTS_AMOUNT)
 
-#define MEM_BLOCK_SIZE (MAX_ALLOCATING_SIZE_IN_TEST*MAX_RUN_ITERATIONS)
+#define HEADER_SIZE 16
+#define MEM_BLOCK_SIZE ((unsigned long)(MAX_ALLOCATING_SIZE_IN_TEST + HEADER_SIZE) * MAX_RUN_ITERATIONS)
 
-static void test(unsigned int iterations_amount, void* (*alloc_fun)(unsigned long), void(*free_fun)(void*)){
-	void* arr[MAX_RUN_ITERATIONS];
-	
-	for (int i = 0; i < iterations_amount; i++)
-			arr[i] = alloc_fun(rand() % MAX_ALLOCATING_SIZE_IN_TEST);
+static void malloc_test(unsigned int iterations_amount){
+	void* arr[MAX_RUN_ITERATIONS] = {NULL};
+	int last_i = 0;
+	for (int i = 0; i < iterations_amount; i++){
+		if (rand()%3 != 0){
+			arr[i] = malloc(rand() % MAX_ALLOCATING_SIZE_IN_TEST);
+			last_i = i;
+		}
+		else if (arr[last_i] != NULL){
+			free(arr[last_i]);
+			arr[last_i] = NULL;
+		}
+	}
 		
 	for (int i = 0; i < iterations_amount; i++)
-		free_fun(arr[i]);
+		free(arr[i]);
+}
+
+static void memalloc_test(unsigned int iterations_amount){
+	void* arr[MAX_RUN_ITERATIONS] = {NULL};
+	int last_i = 0;
+	for (int i = 0; i < iterations_amount; i++){
+		if (rand()%3 != 0){
+			arr[i] = memalloc(rand() % MAX_ALLOCATING_SIZE_IN_TEST);
+			last_i = i;
+		}
+		else if (arr[last_i] != NULL){
+			memfree(arr[last_i]);
+			arr[last_i] = NULL;
+		}
+	}
+	
+	for (int i = 0; i < iterations_amount; i++)
+		memfree(arr[i]);
 }
 
 int main(){
 	setlocale(LC_ALL, "ru_RU.utf8");
+	
 	void* memory_block = malloc(MEM_BLOCK_SIZE);
 	meminit(memory_block, MEM_BLOCK_SIZE);
+	printf("Memory pool was initialized: %lu bytes\n", MEM_BLOCK_SIZE);
 	
 	FILE* result_file = fopen("compare.csv", "w");
 	if (result_file == NULL)
@@ -34,14 +63,13 @@ int main(){
 	
 	unsigned int loop = 0;
 	for (unsigned iters = ITERATIONS_IN_TEST; iters <= MAX_RUN_ITERATIONS; iters+= ITERATIONS_IN_TEST, loop++){
+		printf("\tLoop #%u\n", loop);
 		unsigned long start = clock();
-			test(iters/2, malloc, free);
-			test(iters/2, malloc, free);
+			malloc_test(iters);
 		double std_mem_result = ((double)(clock() - start))/CLOCKS_PER_SEC;
 		
 		start = clock();
-			test(iters/2, memalloc, memfree);
-			test(iters/2, memalloc, memfree);
+			memalloc_test(iters);
 		double lab_mem_result = ((double)(clock() - start))/CLOCKS_PER_SEC;
 		fprintf(result_file, "%d\t%u\t%lf\t%lf\n", loop, iters, std_mem_result, lab_mem_result);
 		
@@ -49,6 +77,5 @@ int main(){
 	memdone();
 	fclose(result_file);
 	free(memory_block);
-	
 	return 0;
 }
