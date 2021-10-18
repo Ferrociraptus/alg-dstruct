5e66c5394865d784f03c990c23b50e519edb0a55
Arseniy Alexeev <ars@alexeev.info>
2021-10-18 22:36:46 +0300

    add: main.c added

---
+++ 2-Alexeev-D28/src/main.c
@@ -0,0 +1,9 @@
+#include "train_task_solving.h"
+
+#define INPUT_FILE_NAME "input.txt"
+#define OUTPUT_FILE_NAME "output.txt"
+
+int main(){
+	organize_train_carriges(INPUT_FILE_NAME, OUTPUT_FILE_NAME);
+	return 0;
+}
