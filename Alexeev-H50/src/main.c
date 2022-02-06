9206199b9f4e9ff9e3c03b8294779b24ca0e2b6a
Arseniy Alexeev <ars@alexeev.info>
2022-02-06 07:42:56 +0300

    add: main.c file added

---
+++ Alexeev-H50/src/main.c
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <locale.h>
+
+#include "BinaryTree.h"
+#include "AABinaryTree.h"
+
+#define NUMBERS_VALUE_RANGE 1000
+#define TESTS_AMOUNT 100
+#define OPERATIONS_MULTYPLAYER 10000
+
+int main(){
+	setlocale(LC_ALL, "ru_Ru");
+	srand(time(NULL));
+	
+	FILE* csv_table = fopen("Compare_table.csv", "wr");
+	fprintf(csv_table, "Test number\tOperations amount\t"					\
+					"UsualBinaryTree Insertion\tAABinaryTree Insertion\t"	\
+					"UsualBinaryTree Searching\tAABinaryTree Searching\t"	\
+					"UsualBinaryTree Removing\tAABinaryTree Removing\n");
+	
+	BinaryTree tree = NULL;
+	AABinaryTree aa_tree = NULL;
+	
+	for (int i = 1; i <= TESTS_AMOUNT; i++){
+		clock_t begin, end;
+		double usual_binary_tree_time_result, aa_binary_tree_time_result;
+		
+		unsigned long operations_amount = i * OPERATIONS_MULTYPLAYER;
+		
+		fprintf(csv_table, "%d\t%lu\t", i, operations_amount);
+		
+		// Insertion check
+		{
+			begin = clock();
+			for (int i = 0; i < operations_amount; i++)
+				binary_tree_insert(&tree, rand() % NUMBERS_VALUE_RANGE);
+			end = clock();
+			usual_binary_tree_time_result = (double)(end - begin) / CLOCKS_PER_SEC;
+			
+			begin = clock();
+			for (int i = 0; i < operations_amount; i++)
+				aa_binary_tree_insert(&aa_tree, rand() % NUMBERS_VALUE_RANGE);
+			end = clock();
+			aa_binary_tree_time_result = (double)(end - begin) / CLOCKS_PER_SEC;
+		
+			fprintf(csv_table, "%lf\t%lf\t", usual_binary_tree_time_result, aa_binary_tree_time_result);
+		}
+		
+		// Searching check
+		{
+			begin = clock();
+			for (int i = 0; i < operations_amount; i++)
+				binary_tree_search(tree, rand() % NUMBERS_VALUE_RANGE);
+			end = clock();
+			usual_binary_tree_time_result = (double)(end - begin) / CLOCKS_PER_SEC;
+			
+			begin = clock();
+			for (int i = 0; i < operations_amount; i++)
+				aa_binary_tree_search(aa_tree, rand() % NUMBERS_VALUE_RANGE);
+			end = clock();
+			aa_binary_tree_time_result = (double)(end - begin) / CLOCKS_PER_SEC;
+		
+			fprintf(csv_table, "%lf\t%lf\t", usual_binary_tree_time_result, aa_binary_tree_time_result);
+		}
+		
+		// Removing check
+		{
+			begin = clock();
+			for (int i = 0; i < operations_amount; i++)
+				binary_tree_search(tree, rand() % NUMBERS_VALUE_RANGE);
+			end = clock();
+			usual_binary_tree_time_result = (double)(end - begin) / CLOCKS_PER_SEC;
+			
+			begin = clock();
+			for (int i = 0; i < operations_amount; i++)
+				aa_binary_tree_search(aa_tree, rand() % NUMBERS_VALUE_RANGE);
+			end = clock();
+			aa_binary_tree_time_result = (double)(end - begin) / CLOCKS_PER_SEC;
+		
+			fprintf(csv_table, "%lf\t%lf\n", usual_binary_tree_time_result, aa_binary_tree_time_result);
+		}
+		binary_tree_clear(&tree);
+		aa_binary_tree_clear(&aa_tree);
+	}
+	fclose(csv_table);
+	return 0;
+}
